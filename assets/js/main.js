18c4df325f2319bd0db75ca9e7bab57060bd8756
GwangGeun <jgg0328@gmail.com>
2019-11-16 20:02:45 +0900

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+import { handleMessageNotif } from "./chat";
+
+// eslint-disable-next-line no-undef
+const socket = io("/");
+
+function sendMessage(message) {
+  socket.emit("newMessage", { message });
+  console.log(`You: ${message}`);
+}
+
+function setNickname(nickname) {
+  socket.emit("setNickname", { nickname });
+}
+
+socket.on("messageNotif", handleMessageNotif);
+
+setNickname("Test");
+sendMessage("hi");
